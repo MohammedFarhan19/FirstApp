cb6d20ec93e7461825b64e92dcd6b4e478f2f594
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-17 13:43:43 +0200

    Calculating Angle between Clock Arms

--- FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
+++ FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
@@ -29,32 +29,63 @@ public class FirstApp {
 //		System.out.println("Mul: " + multiplicationResult);
 //		System.out.printf("Div: %.2f\n" , divisionResult);
 
+		///////////////////////////////////////////////////////////
+		//      Calculating Angle between Clock Arms | START     //
+		//////////////////////////////////////////////////////////
+	
 		// INPUT
-		int h = 11;
-		int m = 15;
-		float angle = 0;
-		boolean error = false;
-
-		// PROCESSING
-		if (h < 12 && m < 59) {
-//			error = true;
-			float minutesArm = m * 6;
-			float hoursArm = h * 30 + minutesArm * 0.5f;
-			angle = hoursArm - minutesArm;
-			if (hoursArm < minutesArm) {
-				angle *= -1;
-			}
-
-			// OUTPUT
-			if (h > 12) {
-				System.out.println("ERROR: hour's can't be greater than 12");
-			} else if (m > 59) {
-				System.out.println("ERROR: minutes can't be greater than 60");
-			}
-
-			else {
-				System.out.println("Angle: " + angle);
-			}
-		}
+//		int h = 23;
+//		int m = 59;
+////		boolean error = false;
+//
+//		// PROCESSING
+//		float angle = 0;
+//		if(h > 12 && h < 24) {
+//			h = h - 12;
+//		}
+//		
+//		if ((h >= 0 && h <= 12) && (m >= 0 && m <= 59)) {
+////			error = true;
+//			float minutesArm = m * 6;
+//			float hoursArm = h * 30 + m * 0.5f;
+//			angle = (hoursArm - minutesArm) % 360;
+//			if (hoursArm < minutesArm) {
+//				angle *= -1;
+//			}
+//		}
+//		
+//		// OUTPUT
+//		if (h > 12) {
+//			System.out.println("ERROR: hour's can't be greater than 12");
+//		} else if (h < 0) {
+//			System.out.println("ERROR: hour's can't be less than 0");
+//		} else if (m < 0) {
+//			System.out.println("ERROR: minutes can't be less than 0");
+//		} else if (m > 59) {
+//			System.out.println("ERROR: minutes can't be greater than 60");
+//		} else {
+//			System.out.println("Angle: " + angle);
+//		}
+		
+		///////////////////////////////////////////////////////////
+		//      Calculating Angle between Clock Arms | END      //
+		//////////////////////////////////////////////////////////
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
