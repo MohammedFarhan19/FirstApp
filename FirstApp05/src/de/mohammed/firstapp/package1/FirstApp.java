8fa23aa2dba44e6d090f1cc560c2dd458a73253d
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-18 09:18:26 +0200

    remove function outside the main but still inside the class

--- FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
+++ FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
@@ -6,110 +6,117 @@
 package de.mohammed.firstapp.package1;
 
 public class FirstApp {
-
-	public static void main(String[] args) {
-
-//		int firstNumber;
-//		firstNumber = 20;
-//		
-//		int secondNumber = 15;
-//		int sum;
-//		sum = firstNumber + secondNumber;
-//		
-//		int diff;
-//		diff = firstNumber - secondNumber;
-//		
-//		int multiplicationResult = firstNumber * secondNumber;
-//		float divisionResult = (float)firstNumber / secondNumber;
-//		
-//		System.out.println("Sum: " + sum);
-//		System.out.println("Diff: " + diff);
-//		System.out.println("Mul: " + multiplicationResult);
-//		System.out.printf("Div: %.2f\n" , divisionResult);
-
+	
+	public static void test() {
+		// Param (requiered false)
+		// Body of the function
+		int firstNumber;
+		firstNumber = 20;
+		
+		int secondNumber = 15;
+		int sum;
+		sum = firstNumber + secondNumber;
+		
+		int diff;
+		diff = firstNumber - secondNumber;
+		
+		int multiplicationResult = firstNumber * secondNumber;
+		float divisionResult = (float)firstNumber / secondNumber;
+		
+		System.out.println("Sum: " + sum);
+		System.out.println("Diff: " + diff);
+		System.out.println("Mul: " + multiplicationResult);
+		System.out.printf("Div: %.2f\n" , divisionResult);
+	}
+	
+	public static String calculateAngleBetweenClockArms(int h, int m) {
+		
 		///////////////////////////////////////////////////////////
 		//      Calculating Angle between Clock Arms | START     //
 		//////////////////////////////////////////////////////////
-	
+
 		// INPUT
 //		int h = 23;
 //		int m = 59;
-////		boolean error = false;
-//
-//		// PROCESSING
-//		float angle = 0;
-//		if(h > 12 && h < 24) {
-//			h = h - 12;
-//		}
-//		
-//		if ((h >= 0 && h <= 12) && (m >= 0 && m <= 59)) {
-////			error = true;
-//			float minutesArm = m * 6;
-//			float hoursArm = h * 30 + m * 0.5f;
-//			angle = (hoursArm - minutesArm) % 360;
-//			if (hoursArm < minutesArm) {
-//				angle *= -1;
-//			}
-//		}
-//		
-//		// OUTPUT
-//		if (h > 12) {
+		//boolean error = false;
+
+		// PROCESSING
+		float angle = 0;
+		if (h > 12 && h < 24) {
+			h = h - 12;
+		}
+
+		if ((h >= 0 && h <= 12) && (m >= 0 && m <= 59)) {
+			//error = true;
+			float minutesArm = m * 6;
+			float hoursArm = h * 30 + m * 0.5f;
+			angle = (hoursArm - minutesArm) % 360;
+			if (hoursArm < minutesArm) {
+				angle *= -1;
+			}
+		}
+
+		// OUTPUT
+		String message = "";
+		if (h > 12) {
+			message = "ERROR: hour's can't be greater than 12";
 //			System.out.println("ERROR: hour's can't be greater than 12");
-//		} else if (h < 0) {
+		} else if (h < 0) {
+			message = "ERROR: hour's can't be less than 0";
 //			System.out.println("ERROR: hour's can't be less than 0");
-//		} else if (m < 0) {
+		} else if (m < 0) {
+			message = "ERROR: minutes can't be less than 0";
 //			System.out.println("ERROR: minutes can't be less than 0");
-//		} else if (m > 59) {
+		} else if (m > 59) {
+			message = "ERROR: minutes can't be greater than 60";
 //			System.out.println("ERROR: minutes can't be greater than 60");
-//		} else {
+		} else {
+			message = "Angle: " + angle;
 //			System.out.println("Angle: " + angle);
-//		}
-		
+		}
+		return message;
+
 		///////////////////////////////////////////////////////////
 		//      Calculating Angle between Clock Arms | END      //
 		//////////////////////////////////////////////////////////
+	}
 
-		
+	public static int calculateBerma() {
 		///////////////////////////////////////////////////////////
 		//            Calculating Berma | START                  //
 		//////////////////////////////////////////////////////////
 		
-		
-//		for(int number = 60; ; number+= 60){
-//			if(	(number+1)  % 7 == 0 ) {
+		for (int number = 60;; number += 60) {
+			if ((number + 1) % 7 == 0) {
 //				System.out.println(number + 1);
+				return number + 1; // return end the function
 //				break;
-//			}
-//		}
+			}
+		}
 		///////////////////////////////////////////////////////////
 		//            Calculating Berma | END                   //
 		//////////////////////////////////////////////////////////
-
+	}
+	
+	public static String getMonthDaysCount(int month, int year) {
 		///////////////////////////////////////////////////////////
 		//            Calculating Days count using IF| START     //
 		//////////////////////////////////////////////////////////
 		
-//		// INPUT
+		// INPUT
 //		int month = 5;
 //		int year = 2045;
-//		
-//		int daysCount;
-//		
-//		//PROCESSING
-//		if(    month == 1 
-//			|| month == 3
-//			|| month == 5
-//			|| month == 7
-//			|| month == 8
-//			|| month == 10
-//			|| month == 12
-//				) {
+
+		int daysCount;
+
+		// PROCESSING
+//		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
 //			daysCount = 31;
-//			
-//		} else if(month == 4 || month == 6 || month == 9 || month == 11) {
+//
+//		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
 //			daysCount = 30;
-//		} else if(month == 2){
-//			if(year % 4 == 0) {
+//		} else if (month == 2) {
+//			if (year % 4 == 0) {
 //				daysCount = 29;
 //			} else {
 //				daysCount = 28;
@@ -117,84 +124,71 @@ public class FirstApp {
 //		} else {
 //			daysCount = 0;
 //		}
-//		
-//		// OUTPUT
-//		if(daysCount == 0) {
-//			System.out.println("INVALID MONTH");
-//		} else {
-//			System.out.println("The month " + month + " is: " + daysCount + " days.");
-//		}
+//
+
 		
 		///////////////////////////////////////////////////////////
 		//            Calculating Days count | END              //
 		//////////////////////////////////////////////////////////
-
-		
-		/*
-		 * *****************************************************************************
-		 */
-		
 		
 		///////////////////////////////////////////////////////////
 		//       Calculating Days count using SWITCH| START     //
 		//////////////////////////////////////////////////////////
-
+		
 		// INPUT
-//		
-//		int month = 122;
-//		int year = 2020;
-//		
-//		int daysCount;
-//		
-//		// PROCESSING
-//		switch(month) {
-//			case 1:
-//			case 3:
-//			case 5:
-//			case 7:
-//			case 8:
-//			case 10:
-//			case 12:
-//				daysCount = 31;
-//				break;
-//			case 4:
-//			case 6:
-//			case 9:
-//			case 11:
-//				daysCount = 31;
-//				break;
-//			case 2:
-//				if(year % 4 == 0) {
-//					daysCount = 29;
-//				} else {
-//					daysCount = 28;
-//				}
-//				break;
-//			default:
-//				daysCount = 0;
-//		}
-//		
-//		// OUTPUT
-//		if(daysCount == 0) {
-//			System.out.println("INVALID MONTH");
-//		} else {
-//			System.out.println("The month " + month + " is: " + daysCount + " days.");
-//		}
-		
-		///////////////////////////////////////////////////////////
-		//       Calculating Days count using SWITCH| END        //
-		//////////////////////////////////////////////////////////
+		//int month = 122;
+		//int year = 2020;
+		//int daysCount;
 		
 		
-		System.out.println("********************* using for ***********************************");
-		// using for
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i);
+		// PROCESSING
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			daysCount = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			daysCount = 31;
+			break;
+		case 2:
+			if (year % 4 == 0) {
+				daysCount = 29;
+			} else {
+				daysCount = 28;
+			}
+			break;
+		default:
+			daysCount = 0;
 		}
+		
+		// OUTPUT
+		String output = "";
+		if (daysCount == 0) {
+			output = "INVALID MONTH";
+//			System.out.println("INVALID MONTH");
+		} else {
+			output = "The month " + month + " is: " + daysCount + " days.";
+//			System.out.println("The month " + month + " is: " + daysCount + " days.");
+		}
+		return output;
 
-		System.out.println("********************* using while ***********************************");
+	}
+	
+	public static void printStars(String functionName) {
+		System.out.println("*********************** " + functionName+ " *************************");
+	}
+	
+	public static String getNumberInfo(int number) {
 		// INPUT
-		int number = 12;
+//		int number = 12;
 		int digitsCount = 0;
 		int digitsSum = 0;
 		int newNumber = 0;
@@ -217,8 +211,6 @@ public class FirstApp {
 //		System.out.println("Digits Count is: " + digitsCount);
 //		System.out.println("Digits Sum is: " + digitsSum);
 		
-		System.out.println("********************* using do while ***********************************");
-		
 		do {
 			int digit = number % 10;
 			System.out.println(digit);
@@ -231,10 +223,30 @@ public class FirstApp {
 		} while (number != 0);
 		
 		// OUTPUT
-		System.out.println("Digits Count is: " + digitsCount);
-		System.out.println("Digits Sum is: " + digitsSum);
-		System.out.println("New Number is: " + newNumber);
-		System.out.println("***************** END *******************");
+		// output is three messsages (return just one Info)
+//		System.out.println("Digits Count is: " + digitsCount);
+//		System.out.println("Digits Sum is: " + digitsSum);
+//		System.out.println("New Number is: " + newNumber);
+//		System.out.println("***************** END *******************");
+		
+		String output = "Digits Count is\t" + digitsCount;
+		output += "\nDigits Sum is\t" + digitsSum;
+		output += "\nNew Number is\t" + newNumber;
+		return output;
+	}
+	
+	public static void main(String[] args) {
+//		printStars("Angle between Clock arms");
+//		String result = calculateAngleBetweenClockArms(11, 30); //function call
+//		System.out.println(result);
+//		printStars("test function");
+//		test();
+//		printStars("Berma function");
+//		System.out.println("Berma Result: " + calculateBerma());
+//		printStars("Days in Month");
+//		System.out.println(getMonthDaysCount(12, 2103));
+//		printStars("Number Info");
+		System.out.println(getNumberInfo(222));
 	}
 }
 
