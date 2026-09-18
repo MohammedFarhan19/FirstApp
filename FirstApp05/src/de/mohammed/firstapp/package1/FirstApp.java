c6901792675609b4e680b17296039f4e087dec68
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-18 14:14:57 +0200

    Using ArrayEngine Class

--- FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
+++ FirstApp05/src/de/mohammed/firstapp/package1/FirstApp.java
@@ -8,23 +8,10 @@ package de.mohammed.firstapp.package1;
 public class FirstApp {
 
 	public static void main(String[] args) {
-		
+
+		ArrayEngine arrayEngine = new ArrayEngine();
+		int[] numbers = { 0, 2, 0, 3, 2, 0, 5 };
+		int[] result = arrayEngine.getDistinctValues(numbers);
+		arrayEngine.printIntArrayUsingForLoop(result);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
